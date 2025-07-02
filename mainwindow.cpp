136ae36f7f645eaa048ce28abc1b0c59a53442ab
jinhyeokjeon <jinhyeokjeoon@gmail.com>
2025-07-02 17:29:41 +0900

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,16 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setFixedSize(480, 320);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
