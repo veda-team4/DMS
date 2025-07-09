455c2bedc6032040e78b33067c64108e7d415aef
jinhyeokjeon <jinhyeokjeoon@gmail.com>
2025-07-09 23:28:47 +0900

    add help.txt

--- install.sh
+++
@@ -1,6 +0,0 @@
-# QT 설치
-sudo apt-get install qtcreator qtbase5-dev qtchooser qtbase5-dev-tools cmake
-
-# 한글 폰트 설치
-# https://m.blog.naver.com/mythee1/223300805398
-# https://blog.naver.com/rlrkcka/223513392314
