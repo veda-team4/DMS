f6751822bb20ade737032bc5e344f5480feabd93
HAN_SH <hansc59220@gmail.com>
2025-07-14 18:11:49 +0900

    c to c++

--- dms_client/include/bluetooth.h
+++ dms_client/include/bluetooth.h
@@ -5,6 +5,20 @@ class Bluetooth {
 public:
   Bluetooth();
   ~Bluetooth();
+  
+private:
+int fd;
+char msg[10] = "RX\n";
+char buf[30];
+void Init();
+void HandleTx();
+void HandleRx();
+
+public:
+int tx_flag = 0;
+void Close();
+void TxFunc();
+void RxFunc();
 };
 
 #endif
--- dms_client/src/bluetooth.cpp
+++ dms_client/src/bluetooth.cpp
@@ -1,7 +1,85 @@
 #include "bluetooth.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <termios.h>
+#include <sys/select.h>
+
+#define BT_DEVICE "/dev/rfcomm0"
+
 
 Bluetooth::Bluetooth() {
+    Init();
 }
 
 Bluetooth::~Bluetooth() {
+}
+
+
+void Bluetooth::Init() {
+    int fd = open("/dev/rfcomm0", O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1) {
+        throw std::runtime_error("Failed to open port\n");
+    }
+
+    struct termios options;
+    tcgetattr(fd, &options);
+
+    cfsetispeed(&options, B9600);
+    cfsetospeed(&options, B9600);
+
+    options.c_cflag |= (CLOCAL | CREAD);    // 로컬 연결, 수신 가능 설정
+    options.c_cflag &= ~PARENB;             // 패리티 없음
+    options.c_cflag &= ~CSTOPB;             // 1 stop bit
+    options.c_cflag &= ~CSIZE; 		        //비트 크기 초기화
+    options.c_cflag |= CS8;                 // 8bit 데이터
+
+    // options.c_cflag &= ~CRTSCTS;
+    
+    // options.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+    // options.c_iflag &= ~(IXON | IXOFF | IXANY);
+    // options.c_oflag &= ~OPOST;
+
+    tcsetattr(fd, TCSANOW, &options);	//설정 내용 즉시 적용
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(300));
+}
+
+void Bluetooth::Close() {
+    close(fd);
+    return 0;
+}
+void Bluetooth::HandleTx() {
+    write(fd, msg, strlen(msg));
+    cout << "Tx : " << msg << endl;
+}
+void Bluetooth::HandleRx() {
+    int n = read(fd, buf, sizeof(buf) - 1);
+    if (n > 0) {
+        buf[n] = '\0';
+        cout << "Rx : " << buf << endl;
+    }
+}
+void Bluetooth::TxFunc() {
+    if (tx_flag == 1) {
+        tx_flag = 0;
+        HandleTx();
+    }
+}
+void Bluetooth::RxFunc() {
+    fd_set read_fds;
+    struct timeval timeout;
+
+    FD_ZERO(&read_fds);
+    FD_SET(fd, &read_fds);
+
+    timeout.tv_sec = 1;
+    timeout.tv_usec = 0;
+
+    int ret = select(fd + 1, &read_fds, NULL, NULL, &timeout);
+    if (ret > 0 && FD_ISSET(fd, &read_fds)) {
+        HandleRx();
+    }
 }
