e3b54bca35e210b219c9bc651120e06ea5333b39
HAN_SH <hansc59220@gmail.com>
2025-07-15 17:19:11 +0900

    success few times

--- dms_client/include/bluetooth.h
+++ dms_client/include/bluetooth.h
@@ -9,14 +9,14 @@ public:
 private:
 int fd;
 char msg[10] = "RX\n";
-char buf[30];
+char buf[18] = {0};
 void Init();
+void Close();
 void HandleTx();
 void HandleRx();
 
 public:
 int tx_flag = 0;
-void Close();
 void TxFunc();
 void RxFunc();
 };
