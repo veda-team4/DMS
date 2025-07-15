958e4c167bdc034141c5f3033ba97c3681891f16
junghwalee <dlwdjghk06@gmail.com>
2025-07-15 11:04:51 +0900

    Add Speaker feature - play

--- dms_client/include/speaker.h
+++ dms_client/include/speaker.h
@@ -1,10 +1,18 @@
 #ifndef SPEAKER_H
 #define SPEAKER_H
 
+#include <string>
+
 class Speaker {
 public:
-  Speaker();
+  Speaker(const std::string& dev = "plughw:3,0");
   ~Speaker();
+
+private:
+  std::string device;
+
+public:
+    void play(const std::string& filename);
 };
 
 #endif
--- dms_client/src/speaker.cpp
+++ dms_client/src/speaker.cpp
@@ -1,7 +1,26 @@
 #include "speaker.h"
+#include <cstdlib>
+#include <string>
+#include <iostream>
 
-Speaker::Speaker() {
+Speaker::Speaker(const std::string& dev) : device(dev) {
+    std::cout << "[INIT] Speaker construction! device = " << device << std::endl;
 }
 
 Speaker::~Speaker() {
+    std::cout << "[INIT] Speaker desturction! device = " << device << std::endl;
 }
+
+void Speaker::play(const std::string& filename)
+{
+    std::string cmd = "aplay -D" + device + " \"" + "./../../dms_client/resources/sounds/" + filename + "\"";
+    std::cout << "[play] start " << filename << "!!" << std::endl;
+
+    int ret = system(cmd.c_str());
+    if(ret == 0){
+        std::cout << "[play] 재생 완료" << std::endl;
+    }
+    else{
+        std::cout << "[play] 재생 실패" << std::endl;
+    }
+} 
