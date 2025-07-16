0ddc7825286b95f7bba0a147ac9f9aaa754b03e1
junghwalee <dlwdjghk06@gmail.com>
2025-07-16 15:26:42 +0900

    Add LED class - led on / led off

--- dms_client/include/led.h
+++ dms_client/include/led.h
@@ -1,10 +1,22 @@
 #ifndef LED_H
 #define LED_H
+#define DMS_MAGIC  'l'
+#define DMS_MAXNR   6
+
+#define DMS_LED_OFF _IO(DMS_MAGIC, 0)
+#define DMS_LED_ON  _IO(DMS_MAGIC, 1)
 
 class Led {
 public:
   Led();
   ~Led();
+
+private:
+  int fd;
+
+public:
+  void led_on();
+  void led_off();
 };
 
 #endif
--- dms_client/src/led.cpp
+++ dms_client/src/led.cpp
@@ -1,7 +1,34 @@
 #include "led.h"
+#include <iostream>
+#include <unistd.h> // close, usleep 등 시스템 호출
+#include <fcntl.h> // for open 
+#include <sys/ioctl.h> // for ioctl
+#include <stdlib.h> // for exit
 
 Led::Led() {
+    std::cout << "[led] led constructor ~" << std::endl;
+    // Open device file
+    fd = open("/dev/dms_led", O_RDWR);
+    std::cout << "fd = " << fd << std::endl;
+
+    if(fd < 0){
+        std::cerr << "/dev/dms_led open error !!!" << std::endl;
+        std::cout << "[led] please check if /dev/dms_led exist !!!" << std::endl;
+        exit(-1);
+    }
 }
 
 Led::~Led() {
+    close(fd);
+    std::cout << "[led] led desturctor ~" << std::endl;
+}
+
+void Led::led_on() {
+    ioctl(fd, DMS_LED_ON, 0);
+    std::cout << "[led] led on !!" << std::endl;
+}
+
+void Led::led_off() {
+    ioctl(fd, DMS_LED_OFF, 0);
+    std::cout << "[led] led off !!" << std::endl;
 }
